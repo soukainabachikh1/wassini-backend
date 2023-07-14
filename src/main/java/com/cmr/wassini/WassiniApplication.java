fb0abf0f3d09d8fbcb1fe83cba117d6f20f9c42d
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-07-14 22:14:28 +0100

    Initializing the project

---
+++ src/main/java/com/cmr/wassini/WassiniApplication.java
@@ -0,0 +1,13 @@
+package com.cmr.wassini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WassiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WassiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmr/wassini/WassiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmr.wassini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WassiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
