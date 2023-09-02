a55dc5cced09da1de33bc68facd9b7105d045611
soukainabachikh1 <bachikh.soukaina@inemail.ine.inpt.ma>
2023-09-03 00:02:31 +0100

    add getters and setters for Reservation

--- src/main/java/com/cmr/wassini/Models/Reservation.java
+++ src/main/java/com/cmr/wassini/Models/Reservation.java
@@ -39,4 +39,84 @@ public class Reservation {
 
     @Column
     private Time endHour;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomComplet() {
+        return nomComplet;
+    }
+
+    public void setNomComplet(String nomComplet) {
+        this.nomComplet = nomComplet;
+    }
+
+    public String getNomCompletAr() {
+        return nomCompletAr;
+    }
+
+    public void setNomCompletAr(String nomCompletAr) {
+        this.nomCompletAr = nomCompletAr;
+    }
+
+    public String getCinDefunt() {
+        return cinDefunt;
+    }
+
+    public void setCinDefunt(String cinDefunt) {
+        this.cinDefunt = cinDefunt;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public userRelationships getUserRelationship() {
+        return userRelationship;
+    }
+
+    public void setUserRelationship(userRelationships userRelationship) {
+        this.userRelationship = userRelationship;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Time getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(Time startHour) {
+        this.startHour = startHour;
+    }
+
+    public Time getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(Time endHour) {
+        this.endHour = endHour;
+    }
 }
