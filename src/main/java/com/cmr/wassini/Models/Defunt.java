0f7cdd4aeae74bde3eb4d46f9f30e0aad2969201
soukainabachikh1 <bachikh.soukaina@inemail.ine.inpt.ma>
2023-09-03 00:00:41 +0100

    add getters and setters for Defunt

--- src/main/java/com/cmr/wassini/Models/Defunt.java
+++ src/main/java/com/cmr/wassini/Models/Defunt.java
@@ -12,4 +12,28 @@ public class Defunt {
     private boolean isRetired;
     @Column
     private boolean hasChildren;
+
+    public String getCIN() {
+        return CIN;
+    }
+
+    public void setCIN(String CIN) {
+        this.CIN = CIN;
+    }
+
+    public boolean isRetired() {
+        return isRetired;
+    }
+
+    public void setRetired(boolean retired) {
+        isRetired = retired;
+    }
+
+    public boolean isHasChildren() {
+        return hasChildren;
+    }
+
+    public void setHasChildren(boolean hasChildren) {
+        this.hasChildren = hasChildren;
+    }
 }
