af28b6ff0cbd3747e72e8e6c536f41c9b7434921
soukainabachikh1 <bachikh.soukaina@inemail.ine.inpt.ma>
2023-09-03 00:01:34 +0100

    add getters and setters for Enfant

--- src/main/java/com/cmr/wassini/Models/Enfant.java
+++ src/main/java/com/cmr/wassini/Models/Enfant.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class Enfant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id_enfant;
+    private long id;
     @Column
     private String nom;
     @Column
@@ -26,4 +26,67 @@ public class Enfant {
     @Column(name = "unique_key_column", unique = true)
     private String cinDefunt;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public boolean isCurrently() {
+        return isCurrently;
+    }
+
+    public void setCurrently(boolean currently) {
+        isCurrently = currently;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+
+    public void setMarried(boolean married) {
+        isMarried = married;
+    }
+
+    public boolean isInfirm() {
+        return isInfirm;
+    }
+
+    public void setInfirm(boolean infirm) {
+        isInfirm = infirm;
+    }
+
+    public InfirmityTypes getInfirmityType() {
+        return infirmityType;
+    }
+
+    public void setInfirmityType(InfirmityTypes infirmityType) {
+        this.infirmityType = infirmityType;
+    }
+
+    public String getCinDefunt() {
+        return cinDefunt;
+    }
+
+    public void setCinDefunt(String cinDefunt) {
+        this.cinDefunt = cinDefunt;
+    }
 }
