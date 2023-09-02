d9e8b866debae63af85cb66129fed306509753a8
soukainabachikh1 <bachikh.soukaina@inemail.ine.inpt.ma>
2023-09-02 23:02:02 +0100

    Reservation Model

--- src/main/java/com/cmr/wassini/Models/Reservation.java
+++ src/main/java/com/cmr/wassini/Models/Reservation.java
@@ -1,19 +1,41 @@
 package com.cmr.wassini.Models;
 
 import com.cmr.wassini.enums.userRelationships;
+import jakarta.persistence.*;
 
 import java.sql.Time;
 import java.util.Date;
 
+@Entity
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column
     private String nomComplet;
+
+    @Column
     private String nomCompletAr;
+
+    @Column
     private String cinDefunt;
+
+    @Column
     private String tel;
+
+    @Column
     private userRelationships userRelationship;
+
+    @Column
     private Date startDate;
+
+    @Column
     private Date endDate;
+
+    @Column
     private Time startHour;
+
+    @Column
     private Time endHour;
 }
