ce7a0de99cea717dd22f189d6c47d9ab79b4a868
soukainabachikh1 <bachikh.soukaina@ine.inpt.ma>
2023-09-03 00:23:43 +0100

    add findAll method to reservation repo

--- src/main/java/com/cmr/wassini/Repo/ReservationRepo.java
+++ src/main/java/com/cmr/wassini/Repo/ReservationRepo.java
@@ -2,7 +2,13 @@ package com.cmr.wassini.Repo;
 
 import com.cmr.wassini.Models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ReservationRepo extends JpaRepository<Reservation,Long> {
+    // Retrieve all reservations
+    List<Reservation> findAll();
 
 }
