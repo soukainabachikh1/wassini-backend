62615de0d86ec1d4bda4f0e2e8eb339ff9c7bd56
soukainabachikh1 <bachikh.soukaina@ine.inpt.ma>
2023-09-03 00:34:30 +0100

    add read and create methods to reservationService file

--- src/main/java/com/cmr/wassini/Services/ReservationService.java
+++ src/main/java/com/cmr/wassini/Services/ReservationService.java
@@ -1,7 +1,25 @@
 package com.cmr.wassini.Services;
 
+import com.cmr.wassini.Models.Reservation;
+import com.cmr.wassini.Repo.ReservationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReservationService {
+    private final ReservationRepo reservationRepo;
+
+    @Autowired
+    public ReservationService(ReservationRepo reservationRepo) {
+        this.reservationRepo = reservationRepo;
+    }
+    public List<Reservation> getAllReservations() {
+        return reservationRepo.findAll();
+    }
+
+    public Reservation createReservation(Reservation reservation) {
+        return reservationRepo.save(reservation);
+    }
 }
