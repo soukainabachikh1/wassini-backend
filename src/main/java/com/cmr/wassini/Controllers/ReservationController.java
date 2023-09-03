f15d8d0cee41561e655a9b5c68dbcae3b2396053
soukainabachikh1 <bachikh.soukaina@ine.inpt.ma>
2023-09-03 21:18:47 +0100

    modifying reservation

--- src/main/java/com/cmr/wassini/Controllers/ReservationController.java
+++ src/main/java/com/cmr/wassini/Controllers/ReservationController.java
@@ -38,6 +38,8 @@ public class ReservationController {
             return ResponseEntity.notFound().build();
         }
     }
+
+
     @PostMapping
     public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
         Reservation createdReservation = reservationService.createReservation(reservation);
