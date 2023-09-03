2ea886493cd805a13943b723b3dd7f47118e7da3
ouahline.omaima <ouahline.omaima@ine.inpt.ma>
2023-09-03 20:52:17 +0100

    add relations to models

--- src/main/java/com/cmr/wassini/Models/Defunt.java
+++ src/main/java/com/cmr/wassini/Models/Defunt.java
@@ -2,6 +2,8 @@ package com.cmr.wassini.Models;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "defunt")
 public class Defunt {
@@ -12,9 +14,30 @@ public class Defunt {
     private boolean isRetired;
     @Column
     private boolean hasChildren;
+    @OneToMany(mappedBy = "defunt")
+    private List<Enfant> enfants;
+
+    @OneToMany(mappedBy = "defunt")
+    private List<Conjoint> conjoints;
 
     public String getCIN() {
         return CIN;
+    }
+
+    public List<Enfant> getEnfants() {
+        return enfants;
+    }
+
+    public void setEnfants(List<Enfant> enfants) {
+        this.enfants = enfants;
+    }
+
+    public List<Conjoint> getConjoints() {
+        return conjoints;
+    }
+
+    public void setConjoints(List<Conjoint> conjoints) {
+        this.conjoints = conjoints;
     }
 
     public void setCIN(String CIN) {
